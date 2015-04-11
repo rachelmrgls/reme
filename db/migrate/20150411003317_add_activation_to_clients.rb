e282ba08ab3d9fd06708477629297b3c637a4a98
Rachel Margulies <rachelmrgls@gmail.com>
2015-04-10 21:52:48 -0400

    no authentication by email

--- db/migrate/20150411003317_add_activation_to_clients.rb
+++
@@ -1,7 +0,0 @@
-class AddActivationToClients < ActiveRecord::Migration
-  def change
-    add_column :clients, :activation_digest, :string
-    add_column :clients, :activated, :boolean, default: false
-    add_column :clients, :activated_at, :datetime
-  end
-end
--- db/migrate/20150411010024_add_reset_to_clients.rb
+++
@@ -1,6 +0,0 @@
-class AddResetToClients < ActiveRecord::Migration
-  def change
-    add_column :clients, :reset_digest, :string
-    add_column :clients, :reset_sent_at, :datetime
-  end
-end
